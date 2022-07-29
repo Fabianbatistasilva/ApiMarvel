23a12e7944f52808218bfc64798f9ab295f7aaa7
Fabianbatistasilva <currifabianbatista@gmail.com>
2022-07-29 17:42:24 -0300

    segundo commit

--- api_marvel/urls.py
+++ api_marvel/urls.py
@@ -2,5 +2,7 @@ from django import views
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name='home')
+    path('comicsEspecifico/<int:id>/',views.comicsEspec,name='comicsEspecifico'),
+    path('comics/',views.comics,name=''),
+    path('',views.index,name='home'),
 ]
--- api_marvel/views.py
+++ api_marvel/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 import requests
 # Create your views here.
 def index(request):
+    marvel=requests.get('https://gateway.marvel.com/v1/public/series?ts=1&apikey=998d4fbbf454824314ac967bb64d0158&hash=9c95b9717bfea2eaa49400e14535256c').json()
+    marvel=marvel['data']['results']
+    print(marvel)
+    context={'marvel':marvel}
+    return render(request,'paginas/home.html',context)
+def comics(request):
     marvel=requests.get('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=998d4fbbf454824314ac967bb64d0158&hash=9c95b9717bfea2eaa49400e14535256c').json()
     marvel=marvel['data']['results']
     context={'marvel':marvel}
-    return render(request,'paginas/home.html',context)
+    return render(request,'paginas/comics.html',context)
+def comicsEspec(request,id):
+    marvel=requests.get(f'https://gateway.marvel.com:443/v1/public/comics/{id}?ts=1&apikey=998d4fbbf454824314ac967bb64d0158&hash=9c95b9717bfea2eaa49400e14535256c').json()
+    marvel=marvel['data']['results']
+    print(marvel)
+    context={'marvel':marvel}
+    return render(request,'paginas/comicsEspecifico.html',context)
