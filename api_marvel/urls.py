0a68eeed8364cfffd85810f4b5334d62100bab2c
Fabianbatistasilva <currifabianbatista@gmail.com>
2022-12-30 14:54:29 -0300

    Add Pages

--- api_marvel/urls.py
+++ api_marvel/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('comicsEspecifico/<int:id>/',views.comicsEspec,name='comicsEspecifico'),
-    path('comics/',views.comics,name=''),
+    path('comics/',views.comics,name='comics'),
+    path('Series/',views.Serie,name='serie'),
     path('',views.index,name='home'),
 ]
